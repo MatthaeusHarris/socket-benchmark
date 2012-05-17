145e965a6c07de4bc316766718fe42e0191704b7
Matt Harris <raven@uberduck.net>
2012-05-17 01:01:10 -0600

    More advanced logging.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var socket = require('socket.io').listen(8888);
 var connections = 0;
 var newconnections = 0;
+
+var getCpuCommand = "ps -p " + process.pid + " -u | grep " + process.pid;
+
 socket.set('log level', 1);
 
 socket.on('connection', function (socket) {
@@ -19,7 +22,20 @@ socket.on('connection', function (socket) {
 
 
 setInterval(function() {
+	var child = exec(getCpuCommand, function(error, stdout, stderr) {
+    	var s = stdout.split(/\s+/);
+    	var cpu = s[2];
+	    var memory = s[3];
 
-	console.log("New Connections: " + newconnections + '\t' + "Total Connections: " + connections);
-	newconnections = 0;
+	    var l = [
+	      'N: ' + newconnections,
+	      'C: ' + connections,
+	      'CPU: ' + cpu,
+	      'Mem: ' + memory
+	    ];
+	    newconnections = 0;
+	    console.log(l.join(',\t'));
+	  });
+	//console.log("New Connections: " + newconnections + '\t' + "Total Connections: " + connections);
+	//newconnections = 0;
 }, 1000);
